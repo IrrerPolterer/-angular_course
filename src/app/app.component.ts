e06d9f78092f44efd24aa1670e0244f7f3c0fb3c
Paul Bütof <paul.buetof@righthandrobotics.com>
2022-08-07 00:16:53 +0200

    move styles to inline

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  // styleUrls: ['./app.component.scss']
+  styles: [`
+    h1 {
+      color: olive; 
+      }
+    `]
 })
 export class AppComponent {
   title = 'angular_course';
