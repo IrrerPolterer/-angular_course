ea8280ae389696245a145a4944920792d0c919d5
Paul Bütof <paul.buetof@righthandrobotics.com>
2022-08-07 13:15:33 +0200

    revert custom css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  // styleUrls: ['./app.component.scss']
-  styles: [`
-    h1 {
-      color: olive; 
-      }
-    `]
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'angular_course';
