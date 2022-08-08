7217d43badb8c5526e964b0bda34e65a36e2624a
IrrerPolterer <irrer.polterer@posteo.de>
2022-08-08 22:29:58 +0200

    change serverColor with ngStyle

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
 
 @Component({
-    selector: 'app-server',
-    templateUrl: './server.component.html',
+  selector: 'app-server',
+  templateUrl: './server.component.html',
 })
 export class ServerComponent {
+  serverId = 123;
+  serverStatus = 'offline';
 
-    serverId = 123;
-    serverStatus = 'offline';
+  constructor() {
+    this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline';
+  }
 
+  get serverColor() {
+    return this.serverStatus == 'online' ? 'green' : 'red';
+  }
 }
