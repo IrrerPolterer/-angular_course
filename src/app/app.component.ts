b573c15162d27806e9946443d5cdef877bad69b9
IrrerPolterer <irrer.polterer@posteo.de>
2022-08-08 23:09:02 +0200

    finish assignment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { timestamp } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'angular_course';
+  showDetails = false;
+  toggleLog:number[] = [];
+
+  toggleDetails() {
+    this.showDetails = !this.showDetails;
+    this.toggleLog.push(Date.now());
+  }
 }
