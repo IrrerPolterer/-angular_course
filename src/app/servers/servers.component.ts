ec2a73d005c3fa51341bb6ed47398007bf32df24
IrrerPolterer <irrer.polterer@posteo.de>
2022-08-08 22:22:16 +0200

    Introduce *ngIf

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -3,13 +3,13 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-servers',
   templateUrl: './servers.component.html',
-  styleUrls: ['./servers.component.scss']
+  styleUrls: ['./servers.component.scss'],
 })
 export class ServersComponent implements OnInit {
-
   allowNewServer = false;
-  serverCreationStatus = "";
-  serverName = "ABC-Server";
+  serverCreationStatus = '';
+  serverName = 'ABC-Server';
+  serverCreated = false;
 
   constructor() {
     setTimeout(() => {
@@ -17,15 +17,14 @@ export class ServersComponent implements OnInit {
     }, 2000);
   }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   onAddServer() {
-    this.serverCreationStatus = "Created new server!";
+    this.serverCreated = true;
+    this.serverCreationStatus = 'Created new server!';
   }
 
   // onUpdateServerName(event: any) {
   //   this.serverName = event.target.value;
   // }
-
 }
