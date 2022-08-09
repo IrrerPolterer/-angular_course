7f404b5d70237041fcde47abfed8db3283145d3d
IrrerPolterer <irrer.polterer@posteo.de>
2022-08-09 11:14:21 +0200

    fix typos

--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-recipies',
+  selector: 'app-recipes',
   templateUrl: './recipes.component.html',
   styleUrls: ['./recipes.component.scss'],
 })
