b6412459305fd63208db9db1ec14ee7c0e74e8b7
IrrerPolterer <irrer.polterer@posteo.de>
2022-08-11 20:21:54 +0200

    implement add ingredient button

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  Output,
+  ViewChild,
+  ElementRef,
+  EventEmitter,
+} from '@angular/core';
+import { Ingredient } from 'src/app/shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-edit',
   templateUrl: './shopping-edit.component.html',
-  styleUrls: ['./shopping-edit.component.scss']
+  styleUrls: ['./shopping-edit.component.scss'],
 })
 export class ShoppingEditComponent implements OnInit {
+  @Output() addIngredient = new EventEmitter<Ingredient>();
 
-  constructor() { }
+  @ViewChild('nameInput', { static: false }) nameRef: ElementRef;
+  @ViewChild('amountInput', { static: false }) amountRef: ElementRef;
 
-  ngOnInit(): void {
+  onAdd() {
+    this.addIngredient.emit(
+      new Ingredient(
+        this.nameRef.nativeElement.value,
+        this.amountRef.nativeElement.value
+      )
+    );
   }
+  constructor() {}
 
+  ngOnInit(): void {}
 }
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  EventEmitter,
+  OnInit,
+  Output,
+  ViewChild,
+} from '@angular/core';
 import { Ingredient } from '../shared/ingredient.model';
 
 @Component({
@@ -6,13 +13,13 @@ import { Ingredient } from '../shared/ingredient.model';
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.scss'],
 })
-export class ShoppingListComponent implements OnInit {
+export class ShoppingListComponent {
   ingredients: Ingredient[] = [
     new Ingredient('Apples', 5),
     new Ingredient('Tomatos', 8),
   ];
 
-  constructor() {}
-
-  ngOnInit(): void {}
+  onIngredientAdded(ingredient: Ingredient) {
+    this.ingredients.push(ingredient);
+  }
 }
