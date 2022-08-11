773f1c3f9921e4e1ffdefdca60ca29ac50f797c3
IrrerPolterer <irrer.polterer@posteo.de>
2022-08-11 20:33:11 +0200

    gameControl functionality

--- src/app/game-control/game-control.component.ts
+++ src/app/game-control/game-control.component.ts
@@ -1,15 +1,24 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { TimeInterval } from 'rxjs/internal/operators/timeInterval';
 
 @Component({
   selector: 'app-game-control',
   templateUrl: './game-control.component.html',
   styleUrls: ['./game-control.component.scss'],
 })
-export class GameControlComponent implements OnInit {
-  @Output() gameStart = new EventEmitter();
-  @Output() gameStop = new EventEmitter();
+export class GameControlComponent {
+  @Output() gameIncrement = new EventEmitter<{ increment: number }>();
+  increment = 0;
+  interval: any;
 
-  constructor() {}
+  onGameStart() {
+    this.interval = setInterval(() => {
+      this.increment += 1;
+      this.gameIncrement.emit({ increment: this.increment });
+    }, 1000);
+  }
 
-  ngOnInit(): void {}
+  onGameStop() {
+    clearInterval(this.interval);
+  }
 }
