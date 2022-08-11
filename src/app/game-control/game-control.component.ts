10e8d0104e6f33b76cb5e8b8305e9dd3ddad913b
IrrerPolterer <irrer.polterer@posteo.de>
2022-08-11 20:33:11 +0200

    prepare game controller button functionality

--- src/app/game-control/game-control.component.ts
+++ src/app/game-control/game-control.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-game-control',
   templateUrl: './game-control.component.html',
-  styleUrls: ['./game-control.component.scss']
+  styleUrls: ['./game-control.component.scss'],
 })
 export class GameControlComponent implements OnInit {
+  @Output() gameStart = new EventEmitter();
+  @Output() gameStop = new EventEmitter();
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
