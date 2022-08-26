4cd74de2a0f3b3b4339bc8f89e0f356a35a7a34b
IrrerPolterer <irrer.polterer@posteo.de>
2022-08-26 16:50:08 +0200

    Manage recipes in separate service

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
+import { RecipesService } from '../recipes.service';
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,31 +8,16 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.scss'],
 })
 export class RecipeListComponent implements OnInit {
-  recipes: Recipe[] = [
-    new Recipe(
-      'Tomato Soup',
-      'Delicious Pomodoro...',
-      'https://www.cubesnjuliennes.com/wp-content/uploads/2022/02/Homemade-Tomato-Soup-Recipe.jpg'
-    ),
-    new Recipe(
-      'Blueberry Cake',
-      'Sweet and sticky...',
-      'https://sugarspunrun.com/wp-content/uploads/2022/03/lemon-blueberry-cake-1-of-1-2.jpg'
-    ),
-    new Recipe(
-      'Sourdough Bread',
-      'Like grandma always made it...',
-      'https://www.abeautifulplate.com/wp-content/uploads/2020/02/artisan-sourdough-bread-recipe-1-34-500x500.jpg'
-    ),
-  ];
-
   @Output() recipeSelected = new EventEmitter<Recipe>();
+  recipes: Recipe[];
+
+  constructor(private recipesService: RecipesService) {}
+
+  ngOnInit(): void {
+    this.recipes = this.recipesService.getRecipes();
+  }
 
   onRecipeSelected(recipe: Recipe) {
     this.recipeSelected.emit(recipe);
   }
-
-  constructor() {}
-
-  ngOnInit(): void {}
 }
--- src/app/recipes/recipes.service.ts
+++ src/app/recipes/recipes.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
+import { Recipe } from './recipe.model';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable()
 export class RecipesService {
+  private recipes: Recipe[] = [
+    new Recipe(
+      'Tomato Soup',
+      'Delicious Pomodoro...',
+      'https://www.cubesnjuliennes.com/wp-content/uploads/2022/02/Homemade-Tomato-Soup-Recipe.jpg'
+    ),
+    new Recipe(
+      'Blueberry Cake',
+      'Sweet and sticky...',
+      'https://sugarspunrun.com/wp-content/uploads/2022/03/lemon-blueberry-cake-1-of-1-2.jpg'
+    ),
+    new Recipe(
+      'Sourdough Bread',
+      'Like grandma always made it...',
+      'https://www.abeautifulplate.com/wp-content/uploads/2020/02/artisan-sourdough-bread-recipe-1-34-500x500.jpg'
+    ),
+  ];
 
-  constructor() { }
+  getRecipes() {
+    return this.recipes.slice(); // return deep copy, prevent direct access to the stored array
+  }
 }
